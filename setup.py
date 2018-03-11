adca39e824b03e8bce8d255ad8e220692a50e5e9
ejweber <webericjames@gmail.com>
2018-03-11 10:18:54 -0500

    Added setup.py to easily prepare a new or upgraded Raspberry Pi for use with webersprinkler

---
+++ setup.py
@@ -0,0 +1,48 @@
+import subprocess
+import os
+log = open('install_log.txt', 'w')
+
+def shellDo(command):
+    try:
+        log.write(str(command) + '\n')
+        print('Executing {}...'.format(command), end=' ', flush=True)
+        output = subprocess.check_output(command, universal_newlines=True, 
+                                         stderr=subprocess.STDOUT)
+        log.write(output)
+        print('[OK]')
+        return(output)
+    except subprocess.CalledProcessError as error:
+        log.write(error.output)
+        errorString = ('\nCommand failed with error code {}. See install_log.txt '
+                       'for more details. Aborting...')
+        print(errorString.format(error.returncode))
+        exit()
+
+if os.geteuid() != 0:
+    print('Must run as root. Aborting...')
+    exit()
+        
+# update Apache installation
+shellDo(['sudo', 'apt-get', 'upgrade', 'apache2', '-qq'])
+shellDo(['sudo', 'apt-get', 'upgrade', 'apache2-dev', '-qq'])
+
+# set permissions for Apache to access GPIO pins
+shellDo(['adduser', 'www-data', 'gpio'])
+
+# install required Python modules
+shellDo(['sudo', 'pip3', 'install', 'mod_wsgi'])
+shellDo(['sudo', 'pip3', 'install', 'bottle'])
+
+# configure Apache virtual host to listen on correct port and run scripts
+shellDo(['sudo', 'cp', 'apache2/sprinkler.conf', '/etc/apache2/sites-available'])
+shellDo(['sudo', 'a2ensite', 'sprinkler'])
+
+# configure Apache to load mod_wsgi from the correct Python interpreter
+output = shellDo(['mod_wsgi-express', 'module-config'])
+wsgiLoadFile = open('apache2/wsgi.load', 'w')
+wsgiLoadFile.write(output)
+shellDo(['cp', 'apache2/wsgi.load', '/etc/apache2/sites-available'])
+shellDo(['a2enmod', 'wsgi'])
+
+# restart Apache so changes can take effect
+shellDo(['service', 'apache2', 'restart'])
